4a2ce76909f17e1a6b3bf2ad545a8ae09adb289b
eznoP <78598661+eznoP@users.noreply.github.com>
2025-10-24 10:50:22 -0300

    Save

--- src/files.py
+++ src/files.py
@@ -1,28 +1,3 @@
-
-import json
-import os
-
-
-
-DATA_PATH = os.path.join(os.path.dirname(__file__), '..', 'data')
-
-def load_data(filename):
-    path = os.path.join(DATA_PATH, filename)
-    if not os.path.exists(path):
-        return []
-    try:
-        with open(path, 'r', encoding='utf-8') as file:
-            return json.load(file)
-    except json.JSONDecodeError:
-        print(f"Aviso: {filename} estava vazio ou corrompido. Criando novo.")
-        return []
-
-def save_data(filename, data):
-    path = os.path.join(DATA_PATH, filename)
-    with open(path, 'w', encoding='utf-8') as file:
-        json.dump(data, file, indent=4, ensure_ascii=False)
-
-
 import json         
 import os          # MODÚLO QUE SERVE PARA INTERAGIR COM O SISTEMA OPERACIONAL
 
